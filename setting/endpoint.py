a5da2a1956345c1fdf7a2c6051c09e16ffb98572
eska <sukmaikha4@gmail.com>
2024-08-14 16:20:14 +0700

    connect to Slack

--- setting/endpoint.py
+++ setting/endpoint.py
@@ -11,3 +11,6 @@ api_result_qase_io = host_qase_io + "/result"
 TOKEN_QASE_IO = "f2de245fd3654615f207146e59322a810ebc1e25a640016f2750527f5ad4d6e7"
 PROJECT_CODE_QASE_IS = "PAA"
 TEST_RUN_QASE_IO = 1
+
+#SLACK
+WEBHOOK = "https://hooks.slack.com/services/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"
---
+++ setting/slack_notification.py
@@ -0,0 +1,138 @@
+import json
+from endpoint import WEBHOOK
+import requests
+from datetime import datetime
+
+
+def slack_notif():
+    json_content = open("report_all.json", "r").read()
+    data_json = json.loads(json_content)
+    datetime_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    try:
+        test_passed = data_json.get("summary")["passed"]
+    except:
+        test_passed = "0"
+
+    try:
+        test_failed = data_json.get("summary")["failed"]
+    except:
+        test_failed = "0"
+
+    try:
+        test_total = data_json.get("summary")["total"]
+    except:
+        test_total = "0"
+
+    test_success_rate = float(test_passed) / float(test_total) * 100
+
+    if float(test_failed) > 1:
+        color = "FF1E00"
+    else:
+        color = "3CFF29"
+
+    payload = {
+        "attachments": [
+            {
+                "color": f"{color}",
+                "blocks": [
+                    {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "API AUTOMATION",
+                            "emoji": True
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "fields": [
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Success Test:*\n {test_passed}"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Failed Test:*\n {test_failed}"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "fields": [
+                            {
+                                "type": "mrkdwn",
+                                "text": "*Skipped Test:*\n0"
+                            },
+                            {
+                                "type": "mrkdwn",
+                                "text": f"*Total Test:*\n {test_total}"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "fields": [
+                            {
+                                "type": "mrkdwn",
+                                "text": F"*Success Rate:*\n {test_success_rate}%"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "<https://google.com|Link Report Test>"
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "rich_text",
+                        "elements": [
+                            {
+                                "type": "rich_text_section",
+                                "elements": [
+                                    {
+                                        "type": "text",
+                                        "text": "tested by : SUKMA KURNIA MUSTIKA",
+                                        "style": {
+                                            "italic": True
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "type": "rich_text",
+                        "elements": [
+                            {
+                                "type": "rich_text_section",
+                                "elements": [
+                                    {
+                                        "type": "text",
+                                        "text": f"created at : {datetime_now}",
+                                        "style": {
+                                            "italic": True
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+
+    }
+
+    req = requests.post(WEBHOOK, json=payload)
+
+
+slack_notif()
